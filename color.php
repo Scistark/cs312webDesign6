de5a3adcbf1acbbc1ea903e2c4bf7ce4f518abe9
eli schneckloth <eligarmodon@gmail.com>
2026-04-06 15:50:38 -0600

    fixed errors

--- color.php
+++ color.php
@@ -25,11 +25,11 @@
 
     <form method="POST" action="color.php">
         <label> Rows & Columns (1-26):</label>
-        <input type="number" name="rows" min="1" max="26">
+        <input type="number" name="rows">
         <br>
 
         <label> Number of Colors (1-10):</label>
-        <input type="number" name="colors" min="1" max="10">
+        <input type="number" name="colors">
         <br>
 
         <button type="submit" class="generate-button">Generate</button>
@@ -40,7 +40,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     $rows = intval($_POST['rows']);
     $num_colors = intval($_POST['colors']);
-
+    $errors = [];
+    if ($rows < 1 || $rows > 26) {
+        $errors[] = "error row/colums must be between 1 and 26";
+    }
+    if ($num_colors < 1 || $num_colors > 10) {
+        $errors[] = "error colors must be between 1 and 10";
+    }
+    if (!empty($errors)) {
+        echo "<div style='color: red; font-weight: bold;'>";
+        foreach ($errors as $msg) {
+            echo "<p>$msg</p>";
+        }
+        echo "</div>";
+    } else{
     $color_options = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey", "Brown", "Black", "Teal"];
 
     echo "<h2>Color Selection</h2>";
@@ -87,6 +100,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "</tr>";
     }
     echo "</table>";
+}
 }
 ?>
 
