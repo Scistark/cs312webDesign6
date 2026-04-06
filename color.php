33709b008e2970f555129298f4ae2f167a7d92f3
Chimerabot <gideon@lo-fi.net>
2026-04-06 17:25:30 -0600

    Move color selection to fragment folder

--- color.php
+++ color.php
@@ -44,34 +44,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else{
     $color_options = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey", "Brown", "Black", "Teal"];
 
-    echo "<h2>Color Selection</h2>";
-    
-    echo "<table class='color-table'>";
-    for ($i = 0; $i < $num_colors; $i++) {
-        echo "<tr>";
-        
-        //dropdown
-        echo "<td>";
-        echo "<select>";
-        foreach ($color_options as $index => $name) {
-            $selected = ($i == $index) ? "selected" : "";
-            echo "<option value='$name' $selected>$name</option>";
-        }
-        //leftt block
-        echo "</select>";
-        echo "</td>";
-        
-        //right block
-        echo "<td></td>";
-        echo "</tr>";
-    }
-    echo "</table>";
+    include 'fragments/colorSelection.php';
 
     include 'fragments/coordinateGrid.php';
 }
 }
 ?>
-
+    <br>
     <button> View Printable Version </button>
 
 </body>
