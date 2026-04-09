f52701b83ea94375b201ef7383d52da81837d5ae
Nikolas-Augustini <snowkai@dorado.cs.colostate.edu>
2026-04-08 21:48:18 -0600

    added the rest of the clover styling.

--- index.php
+++ index.php
@@ -25,12 +25,16 @@
         <div class="welcome">
         <h1 > Planning color schemes <br>with ultimate precision </h1>
         </div>
+
         <div class="triangle"></div>
         <div class="triangle-grey"></div>
         <div class="rectangle-nav"></div>
         <div class="rectangle-side"></div>
         <div class="bar-left"></div>
         <div class="bar-right"></div>
+        <div class="rectangle-left"></div>
+        <div class="rectangle-right"></div>
+    
     </main>
     <nav class="navbar2">
      <div class="nav-link">   
